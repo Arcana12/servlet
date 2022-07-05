445af0dad8702a42ff944c42af0927aa449ad8f3
Arcana12 <arcdrive12@gmail.com>
2022-07-05 19:33:23 +0900

    http servlet-request

---
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -0,0 +1,18 @@
+package hello.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.annotation.WebServlet;
+
+@ServletComponentScan  //서블릿 자동 등록
+@SpringBootApplication
+public class ServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServletApplication.class, args);
+	}
+
+}
